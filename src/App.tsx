a27f0e367756904df8137d642f78f9ac7c1b0844
Michel Van Autrève <michel.vanautreve@outlook.com>
2022-05-23 10:36:04 +0200

    QR Code Generator toegevoegd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import { QRCodeSVG } from "qrcode.react";
 
+function App() {
   return (
     <div>
       <h1 className="text-lg text-alpha font-proxima">Kinepolis</h1>
+      <QRCodeSVG value="https://www.kinepolis.com" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
