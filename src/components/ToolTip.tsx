c0a8cedd8e053909877f8530f3abdddc534fec01
Michel Van Autrève <michel.vanautreve@outlook.com>
2022-05-25 11:32:13 +0200

    TooltipUpdate

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -1,10 +1,12 @@
-export default () => {
-    return (
-        <div className="w-36 rounded-2xl py-1.5 px-3.5 bg-alpha-x-light dark:bg-alpha-xx-light">
-            <h1 className="font-proxima font-bold uppercase text-sm text-white text-center">Scan hier en koop je tickets</h1>
-            <div className="w-7 overflow-hidden absolute mt-1.5 ml-16">
-                <div className=" h-5 w-5 -rotate-45 transform origin-top-left bg-alpha-x-light dark:bg-alpha-xx-light"></div>
-            </div>
-        </div>
-    )
-  }
+export default ({ text }: { text: string }) => {
+  return (
+    <div className="w-36 rounded-2xl py-1.5 px-3.5 bg-alpha-x-light dark:bg-alpha-xx-light">
+      <h1 className="font-proxima font-bold uppercase text-sm text-white text-center">
+        {text}
+      </h1>
+      <div className="w-7 overflow-hidden absolute mt-1.5 ml-16">
+        <div className=" h-5 w-5 -rotate-45 transform origin-top-left bg-alpha-x-light dark:bg-alpha-xx-light"></div>
+      </div>
+    </div>
+  )
+}
