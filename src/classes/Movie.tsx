42c686c2c6063025de45abfe1f809bc8ca18f089
Michel Van Autrève <michel.vanautreve@outlook.com>
2022-05-25 16:08:25 +0200

    CleanFetch & RandomSeats & Class
    Cleane fetch gemaakt, Random seats gegenereerd, en Class (met constructor pattern) gemaakt voor movies

---
+++ src/classes/Movie.tsx
@@ -0,0 +1,59 @@
+import { ISession } from '../interfaces/Movies'
+
+export class Movie {
+  constructor({
+    complexOperator,
+    documentType,
+    country,
+    language,
+    showtime,
+    businessDay,
+    hall,
+    vistaSessionId,
+    hasSeatingPlan,
+    hasSpecialSeating,
+    hasCosySeating,
+    isSoldOut,
+    isSneakPreview,
+    isPublicScreening,
+    circuit,
+    film,
+    cinemaLabel,
+    mainComplex,
+    sessionAttributes,
+    id,
+    movie,
+    maxSeats = 250,
+    availableSeats = Math.floor(Math.random() * 250),
+  }: ISession) {
+    Object.assign(this, {
+      complexOperator,
+      documentType,
+      country,
+      language,
+      showtime,
+      businessDay,
+      hall,
+      vistaSessionId,
+      hasSeatingPlan,
+      hasSpecialSeating,
+      hasCosySeating,
+      isSoldOut,
+      isSneakPreview,
+      isPublicScreening,
+      circuit,
+      film,
+      cinemaLabel,
+      mainComplex,
+      sessionAttributes,
+      id,
+      movie,
+      maxSeats,
+      availableSeats,
+    })
+  }
+
+  render() {
+    return <h1>{this.movie.title}</h1>
+  }
+}
