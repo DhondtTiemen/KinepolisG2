9ea6a4c5f633ced69495dd4a5128edc13e13acdd
Ellen Dequeker <ellen.dequeker@student.howest.be>
2022-05-26 16:50:41 +0200

    Fix margin kijkwijzers

--- src/components/cardComponents/MovieTechnology.tsx
+++ src/components/cardComponents/MovieTechnology.tsx
@@ -1,36 +1,41 @@
-import { SessionAttribute } from "../../interfaces/Movies"
+import { SessionAttribute } from '../../interfaces/Movies'
 
-export default ({sessionAttributes}: {sessionAttributes: SessionAttribute[]}) => {
+export default ({
+  sessionAttributes,
+}: {
+  sessionAttributes: SessionAttribute[]
+}) => {
   const baseURL = 'https://cdn.kinepolis.be/images'
 
   return (
-    <div className="flex items-center">
-      {
-        sessionAttributes?.map((attribute) => (
-          <div className="bg-white rounded-full w-4 h-4 mr-2">
-            <img src={`${baseURL}${attribute.imageUrl}`} alt={attribute.shortName}/>
-          </div>
-        ))
-      }
+    <div className="flex gap-2 items-center">
+      {sessionAttributes?.map((attribute) => (
+        <div className="bg-white rounded-full w-4 h-4 ">
+          <img
+            src={`${baseURL}${attribute.imageUrl}`}
+            alt={attribute.shortName}
+          />
+        </div>
+      ))}
     </div>
   )
 }
 
-    // {
-    //   sessionAttributes.map((session) => (
-    //   <div>
-    //     {session.imageUrl}
-    //   </div>
-    // }
-    // <div className="flex items-center">
-    //   <div className="bg-white rounded-full w-4 h-4 mr-2">
-    //     <img src="" alt="" />
-    //   </div>
-    //   <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-4 mr-2"></div>
-    //   <div className="bg-white rounded-full w-4 h-4 mr-1">
-    //     <img src="" alt="" />
-    //   </div>
-    //   <div className="bg-white rounded-full w-4 h-4 ">
-    //     <img src="" alt="" />
-    //   </div>
-    // </div>
+// {
+//   sessionAttributes.map((session) => (
+//   <div>
+//     {session.imageUrl}
+//   </div>
+// }
+// <div className="flex items-center">
+//   <div className="bg-white rounded-full w-4 h-4 mr-2">
+//     <img src="" alt="" />
+//   </div>
+//   <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-4 mr-2"></div>
+//   <div className="bg-white rounded-full w-4 h-4 mr-1">
+//     <img src="" alt="" />
+//   </div>
+//   <div className="bg-white rounded-full w-4 h-4 ">
+//     <img src="" alt="" />
+//   </div>
+// </div>
