f8c581330b6ec3e10dc7f9f0b99d2e062a347aba
DhondtTiemen <tiemen.dhondt2@student.howest.be>
2022-05-26 15:09:21 +0200

    Update small design

--- src/components/cardComponents/MovieTechnology.tsx
+++ src/components/cardComponents/MovieTechnology.tsx
@@ -4,27 +4,15 @@ export default ({sessionAttributes}: {sessionAttributes: SessionAttribute[]}) =>
   const baseURL = 'https://cdn.kinepolis.be/images'
 
   return (
-    <>
+    <div className="flex items-center">
       {
         sessionAttributes?.map((attribute) => (
-          <div>
+          <div className="bg-white rounded-full w-4 h-4 mr-2">
             <img src={`${baseURL}${attribute.imageUrl}`} alt={attribute.shortName}/>
           </div>
         ))
       }
-      <div className="flex items-center">
-        <div className="bg-white rounded-full w-4 h-4 mr-2">
-          <img src="" alt="" />
-        </div>
-        <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-4 mr-2"></div>
-        <div className="bg-white rounded-full w-4 h-4 mr-1">
-          <img src="" alt="" />
-        </div>
-        <div className="bg-white rounded-full w-4 h-4 ">
-         <img src="" alt="" />
-        </div>
-     </div>
-    </>
+    </div>
   )
 }
 
