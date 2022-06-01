07856b3d8f220a9821d6668be34485b1b378a8e8
Ellen Dequeker <ellen.dequeker@student.howest.be>
2022-06-01 11:39:14 +0200

    Fix colors seats & label dark and light mode

--- src/components/cardComponents/MovieLabel.tsx
+++ src/components/cardComponents/MovieLabel.tsx
@@ -14,7 +14,9 @@ export default function MovieLabel({
       <div className="flex justify-end mr-4 -mb-2">
         <div
           className={`w-[90%] h-[22px] ${
-            type == 'popular' ? 'bg-alpha-xx-light' : 'bg-warning'
+            type == 'popular'
+              ? 'dark:bg-alpha-xx-light bg-alpha-x-light'
+              : 'dark:bg-warning bg-error'
           } rounded-b-md `}
         >
           <p className="text-center text-white font-proxima text-xs font-semibold mt-[2px]">
