67ae54f1939c30b6164426e9b3003c59781c0dd4
Ellen Dequeker <ellen.dequeker@student.howest.be>
2022-06-08 20:22:03 +0200

    Fix merge mistake

--- src/components/cardComponents/MovieTechnology.tsx
+++ src/components/cardComponents/MovieTechnology.tsx
@@ -12,86 +12,83 @@ export default function MovieTechnology({
   if (sessionAttributes == undefined) {
     return <div className="h-6 w-6"></div>
   } else {
-  return (
-      {format.name == '4DX 3D'
-    <div className="flex gap-2 items-center">
-        ? sessionAttributes?.map((attribute) =>
-            attribute.shortName != 'Pauze' &&
-            attribute.shortName != 'Familie' &&
-            attribute.shortName != 'Franse F' ? (
-              attribute.shortName == 'LaserUltra' ||
-              attribute.shortName == '3D' ||
-              attribute.shortName == '4DX' ||
-              attribute.shortName == 'IMAX3D' ? (
-                attribute.shortName == '3D' ? (
-                  <div key={attribute.id} className="flex items-center">
-                    <div className="bg-white rounded-full w-6 h-6">
+    return (
+      <div className="flex gap-2 items-center">
+        {format.name == '4DX 3D'
+          ? sessionAttributes?.map((attribute) =>
+              attribute.shortName != 'Pauze' &&
+              attribute.shortName != 'Familie' &&
+              attribute.shortName != 'Franse F' ? (
+                attribute.shortName == 'LaserUltra' ||
+                attribute.shortName == '3D' ||
+                attribute.shortName == '4DX' ||
+                attribute.shortName == 'IMAX3D' ? (
+                  attribute.shortName == '3D' ? (
+                    <div key={attribute.id} className="flex items-center">
+                      <div className="bg-white rounded-full w-6 h-6">
                         src={`${baseURL}${attribute.imageUrl}`}
-                      <img
-                        alt={attribute.shortName}
-                      />
+                        <img alt={attribute.shortName} />
+                      </div>
                     </div>
-                  </div>
+                  ) : (
+                    <div key={attribute.id} className="flex items-center">
+                      <div className="bg-white rounded-full w-6 h-6 mr-2">
+                        <img
+                          src={`${baseURL}${attribute.imageUrl}`}
+                          alt={attribute.shortName}
+                        />
+                      </div>
+                      <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
+                    </div>
+                  )
                 ) : (
-                  <div key={attribute.id} className="flex items-center">
-                    <div className="bg-white rounded-full w-6 h-6 mr-2">
-                      <img
-                        src={`${baseURL}${attribute.imageUrl}`}
-                        alt={attribute.shortName}
-                      />
-                    </div>
-                    <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
+                  <div
+                    className="bg-white rounded-full w-6 h-6 "
+                    key={attribute.id}
+                  >
+                    <img
+                      src={`${baseURL}${attribute.imageUrl}`}
+                      alt={attribute.shortName}
+                    />
                   </div>
                 )
               ) : (
-                  className="bg-white rounded-full w-6 h-6 "
-                <div
-                  key={attribute.id}
-                >
-                  <img
-                    src={`${baseURL}${attribute.imageUrl}`}
-                    alt={attribute.shortName}
-                  />
-                </div>
-              )
-            ) : (
-              ''
-            ),
-          )
-        : sessionAttributes?.map((attribute) =>
-            attribute.shortName != 'Pauze' &&
-            attribute.shortName != 'Familie' &&
-            attribute.shortName != 'Franse F' ? (
-              attribute.shortName == 'LaserUltra' ||
-              attribute.shortName == '3D' ||
-              attribute.shortName == '4DX' ||
-              attribute.shortName == 'IMAX3D' ? (
-                <div key={attribute.id} className="flex items-center">
-                  <div className="bg-white rounded-full w-6 h-6 mr-2">
+                ''
+              ),
+            )
+          : sessionAttributes?.map((attribute) =>
+              attribute.shortName != 'Pauze' &&
+              attribute.shortName != 'Familie' &&
+              attribute.shortName != 'Franse F' ? (
+                attribute.shortName == 'LaserUltra' ||
+                attribute.shortName == '3D' ||
+                attribute.shortName == '4DX' ||
+                attribute.shortName == 'IMAX3D' ? (
+                  <div key={attribute.id} className="flex items-center">
+                    <div className="bg-white rounded-full w-6 h-6 mr-2">
                       src={`${baseURL}${attribute.imageUrl}`}
-                    <img
-                    />
+                      <img />
                       alt={attribute.shortName}
+                    </div>
+                    <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
                   </div>
-                  <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
-                </div>
+                ) : (
+                  <div
+                    className="bg-white rounded-full w-6 h-6 "
+                    key={attribute.id}
+                  >
+                    <img
+                      src={`${baseURL}${attribute.imageUrl}`}
+                      alt={attribute.shortName}
+                    />
+                  </div>
+                )
               ) : (
-                <div
-                  className="bg-white rounded-full w-6 h-6 "
-                  key={attribute.id}
-                >
-                  <img
-                    src={`${baseURL}${attribute.imageUrl}`}
-                    alt={attribute.shortName}
-                  />
-                </div>
-              )
-            ) : (
-              ''
-            ),
-          )}
-    </div>
-  )
+                ''
+              ),
+            )}
+      </div>
+    )
   }
 }
 
