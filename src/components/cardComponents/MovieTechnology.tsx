be9f3975624765eefcb61af66e150eb79b099081
Ellen Dequeker <ellen.dequeker@student.howest.be>
2022-06-08 15:22:29 +0200

    Fix kijkwijzers wanneer er geen zijn

--- src/components/cardComponents/MovieTechnology.tsx
+++ src/components/cardComponents/MovieTechnology.tsx
@@ -6,35 +6,44 @@ export default function MovieTechnology({
   sessionAttributes: SessionAttribute[]
 }) {
   const baseURL = 'https://cdn.kinepolis.be/images'
+  console.log(sessionAttributes)
 
-  return (
-    <div className="flex gap-2 items-center">
-      {sessionAttributes?.map((attribute) =>
-        attribute.shortName != 'Pauze' && attribute.shortName != 'Familie' ? (
-          attribute.shortName == 'LaserUltra' || attribute.shortName == '3D' ? (
-            <div key={attribute.id} className="flex items-center">
-              <div className="bg-white rounded-full w-6 h-6 mr-2">
+  if (sessionAttributes == undefined) {
+    return <div className="h-6 w-6"></div>
+  } else {
+    return (
+      <div className="flex gap-2 items-center">
+        {sessionAttributes?.map((attribute) =>
+          attribute.shortName != 'Pauze' && attribute.shortName != 'Familie' ? (
+            attribute.shortName == 'LaserUltra' ||
+            attribute.shortName == '3D' ? (
+              <div key={attribute.id} className="flex items-center">
+                <div className="bg-white rounded-full w-6 h-6 mr-2">
+                  <img
+                    src={`${baseURL}${attribute.imageUrl}`}
+                    alt={attribute.shortName}
+                  />
+                </div>
+                <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
+              </div>
+            ) : (
+              <div
+                className="bg-white rounded-full w-6 h-6 "
+                key={attribute.id}
+              >
                 <img
                   src={`${baseURL}${attribute.imageUrl}`}
                   alt={attribute.shortName}
                 />
               </div>
-              <div className="bg-alpha-xxx-light opacity-40 dark:bg-gray-xx-light dark:opacity-100 w-[2px] rounded-sm h-5"></div>
-            </div>
+            )
           ) : (
-            <div className="bg-white rounded-full w-6 h-6 " key={attribute.id}>
-              <img
-                src={`${baseURL}${attribute.imageUrl}`}
-                alt={attribute.shortName}
-              />
-            </div>
-          )
-        ) : (
-          ''
-        ),
-      )}
-    </div>
-  )
+            ''
+          ),
+        )}
+      </div>
+    )
+  }
 }
 
 // {sessionAttributes?.map((attribute) => {
